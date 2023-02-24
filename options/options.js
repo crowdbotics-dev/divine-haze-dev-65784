db994a46345afc70ac2f4a83b7033e085e83ab34
Crowdbotics <engineering@crowdbotics.com>
2023-02-24 01:47:37 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "divine-haze-dev-65784",
+  url: "https://divine-haze-dev-65784.botics.co",
+  api: "https://divine-haze-dev-65784.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
